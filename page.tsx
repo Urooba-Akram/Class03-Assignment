6cc9af59430f3dc9a3d01cf04ac836bf94ade3ce
Urooba Akram <159257250+Urooba-Akram@users.noreply.github.com>
2024-10-04 19:33:24 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header"
+
+
+export default function Jobs() {
+    return(
+        <div>
+            <Header/>
+            <h1>This is job</h1>
+        </div>
+    )
+} 
